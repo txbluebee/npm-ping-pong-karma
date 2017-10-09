f776801da000fc3a9e9ec144ae162b2b44a0d95c
Brian Fan <kuomu.fan@yahoo.com>
2017-10-09 14:19:37 -0700

    add beforeEach() jasmine helper

--- spec/pingpong-spec.js
+++ spec/pingpong-spec.js
@@ -1,8 +1,17 @@
 const Calculator = require('./../js/pingpong.js').calculatorModule;
 
 describe('Calculator', ()=>{
+  let testCalculator;
+
+  beforeEach(()=>{
+    testCalculator = new Calculator('blue');
+  });
+
   it('should correctly show skincolor', ()=>{
-    let testCalculator= new Calculator('blue');
     expect(testCalculator.skin).toEqual('blue');
   })
+
+  it('should return [1,2,"ping"] when call pingpong(3)', ()=>{
+    expect(testCalculator.pingPong(3)).toEqual([1,2,'ping']);
+  });
 });
